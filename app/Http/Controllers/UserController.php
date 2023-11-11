766eb05a98517ba3a80a9d3c4ce8c54764bc4574
Md. Mitul Hossain <143118872+mitulhossen@users.noreply.github.com>
2023-11-11 22:07:46 +0600

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+   public function sayHi(){
+    return "Hello, Laravel!";
+   }
+}
+
+
+
+
